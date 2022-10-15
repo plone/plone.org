9b53679fcad4918192adc6bab5c8dd09482d27e6
nzambello <nzambello@protonmail.com>
2022-10-15 16:49:08 +0200

    fix(backend): isort issue

--- backend/src/ploneorg/src/ploneorg/testing.py
+++ backend/src/ploneorg/src/ploneorg/testing.py
@@ -1,9 +1,9 @@
 from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
-from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
 from plone.testing.zope import WSGI_SERVER_FIXTURE
 
 import ploneorg
