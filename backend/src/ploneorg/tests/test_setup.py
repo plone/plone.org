6328d8eb22599318eeba1e1eb3b293b22ea47af5
Érico Andrei <ericof@gmail.com>
2022-11-12 11:23:21 -0300

    Add Workflow for Foundation Member

--- backend/src/ploneorg/tests/test_setup.py
+++ backend/src/ploneorg/tests/test_setup.py
@@ -34,7 +34,7 @@ class TestSetup(unittest.TestCase):
         """Test latest version of default profile."""
         self.assertEqual(
             self.setup.getLastVersionForProfile("ploneorg:default")[0],
-            "20221014001",
+            "20221112001",
         )
 
 
