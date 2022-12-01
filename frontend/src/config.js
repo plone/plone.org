ec0093f68a7374689c9a79a9fd28d3341d6316ad
Philip Bauer <bauer@starzel.de>
2022-12-01 11:39:50 +0100

    update yarn.lock

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export default function applyConfig(config) {
-    config.blocks.blocksConfig.__grid = {
-      ...config.blocks.blocksConfig.__grid,
-      gridAllowedBlocks: ['teaser', 'image', 'slate', 'html'],
-      }
-    return config;
-  }
+  config.blocks.blocksConfig.__grid = {
+    ...config.blocks.blocksConfig.__grid,
+    gridAllowedBlocks: ['teaser', 'image', 'slate', 'html'],
+  };
+  return config;
+}
