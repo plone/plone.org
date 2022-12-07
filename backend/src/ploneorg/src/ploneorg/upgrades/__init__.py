d92f19156d9f7e401e9cc3098ff47095fabfb90d
Érico Andrei <ericof@gmail.com>
2022-12-07 15:28:30 -0300

    Add upgrade step to install plone.app.caching

--- backend/src/ploneorg/src/ploneorg/upgrades/__init__.py
+++ backend/src/ploneorg/src/ploneorg/upgrades/__init__.py
@@ -10,3 +10,11 @@ def install_case_study(setup_tool=None):
     installer = get_installer(portal, getRequest())
     installer.install_product("collective.casestudy")
     logger.info("Installed collective.casestudy")
+
+
+def install_caching(setup_tool=None):
+    """Install plone.app.caching."""
+    portal = api.portal.get()
+    installer = get_installer(portal, getRequest())
+    installer.install_product("plone.app.caching")
+    logger.info("Installed plone.app.caching")
