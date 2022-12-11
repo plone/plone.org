16ba2a2b89d9b2e44a2c35a00d48b9c4481e19be
Fred van Dijk <fredvd@gmail.com>
2022-12-11 16:30:56 +0100

    Update volto-gdpr-privcay to 2.0.0 for Volto 16 Disable sticky right side privacy/user/lock icon Move cookie settings to footer Rearrange social column, add privacy statement and cookie settings link

--- frontend/src/customizations/volto/components/theme/Footer/Footer.jsx
+++ frontend/src/customizations/volto/components/theme/Footer/Footer.jsx
@@ -8,6 +8,11 @@ import { Container, List, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { Logo } from '@plone/volto/components';
 import { defineMessages, injectIntl } from 'react-intl';
+import { displayBanner } from 'volto-gdpr-privacy';
+
+import { useDispatch } from 'react-redux';
+import { LI } from '../../../../../../omelette/packages/volto-slate/src/constants';
+
 
 const messages = defineMessages({
   copyright: {
@@ -19,6 +24,10 @@ const messages = defineMessages({
     defaultMessage:
       'The text and illustrations in this website are licensed by the Plone Foundation under a Creative Commons Attribution-ShareAlike 4.0 International license. Plone and the Plone® logo are registered trademarks of the Plone Foundation, registered in the United States and other countries. For guidelines on the permitted uses of the Plone trademarks, see https://plone.org/foundation/logo. All other trademarks are owned by their respective owners.',
   },
+  cookieSettings: {
+    id: 'Cookie settings',
+    defaultMessage: 'Cookie settings',
+  },
 });
 
 /**
@@ -28,6 +37,10 @@ const messages = defineMessages({
  * @returns {string} Markup of the component
  */
 const Footer = ({ intl }) => {
+
+  const dispatch = useDispatch();
+
+
   return (
     <div id="footer">
       <Container>
@@ -120,46 +133,79 @@ const Footer = ({ intl }) => {
                   <List.Item>
                     <Link to="/foundation/members">Foundation members</Link>
                   </List.Item>
-                </List.Content>
-              </List>
-            </Grid.Column>
-            <Grid.Column>
-              <List inverted>
-                <List.Header>
-                  <Link to="/news-and-events/plone-in-social-media">
-                    Follow us
-                  </Link>
-                </List.Header>
-                <List.Content>
                   <List.Item>
                     <a href="https://plone.teemill.com/">Shop</a>
                   </List.Item>
+
+                </List.Content>
+              </List>
+            </Grid.Column>
+            <Grid.Column className="separated">
+              <List>
+                <List.Header>
+                      <Link to="/news-and-events/plone-in-social-media">
+                        Follow us
+                      </Link>
+                </List.Header>
+                <List.Content>
+                  <Grid textAlign="left" columns={2}>
+                    <Grid.Column>
+                      <List>
+                        <List.Content>
+                          <List.Item>
+                            <a href="https://plone.social/@plone" rel="me">
+                              Mastodon
+                            </a>
+                          </List.Item>
+                          <List.Item>
+                            <a href="https://twitter.com/plone">Twitter</a>
+                          </List.Item>
+                          <List.Item>
+                            <a href="https://www.instagram.com/plonecms/">Instagram</a>
+                          </List.Item>
+                        </List.Content>
+                      </List>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <List>
+                        <List.Content>
+                          <List.Item>
+                            <a href="https://www.youtube.com/@plonecms">YouTube</a>
+                          </List.Item>
+                          <List.Item>
+                            <a href="https://www.linkedin.com/company/plone-foundation/">
+                              Linkedin
+                            </a>
+                          </List.Item>
+                          <List.Item>
+                            <a href="https://www.facebook.com/plonecms">Facebook</a>
+                          </List.Item>
+                        </List.Content>
+                      </List>
+                    </Grid.Column>
+                  </Grid>
+                </List.Content>
+              </List>
+              <List>
+                <List.Header>
+                  <List.Item><Link to="/privacy-statement">Privacy Statement</Link></List.Item>
+                </List.Header>
+                <List.Content>
                   <List.Item>
-                    <a href="https://plone.social/@plone" rel="me">
-                      Mastodon
-                    </a>
-                  </List.Item>
-                  <List.Item>
-                    <a href="https://twitter.com/plone">Twitter</a>
-                  </List.Item>
-                  <List.Item>
-                    <a href="https://www.instagram.com/plonecms/">Instagram</a>
-                  </List.Item>
-                  <List.Item>
-                    <a href="https://www.youtube.com/@plonecms">YouTube</a>
-                  </List.Item>
-                  <List.Item>
-                    <a href="https://www.linkedin.com/company/plone-foundation/">
-                      Linkedin
-                    </a>
-                  </List.Item>
-                  <List.Item>
-                    <a href="https://www.facebook.com/plonecms">Facebook</a>
+                  <a
+                    className=""
+                    onClick={(e) => {
+                      dispatch(displayBanner(true, true));
+                    }}
+                    title={intl.formatMessage(messages.cookieSettings)}
+                  >
+                    {intl.formatMessage(messages.cookieSettings)}
+                  </a>
                   </List.Item>
                 </List.Content>
               </List>
             </Grid.Column>
-          </Grid>
+            </Grid>
         </div>
       </Container>
       <div id="footer-small-wrapper">
