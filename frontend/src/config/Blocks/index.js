af54106bed4a51103ce11dd38849d59042fe4c2c
Érico Andrei <ericof@gmail.com>
2022-12-12 01:54:07 -0300

    Implement SponsorCard listing

--- frontend/src/config/Blocks/index.js
+++ frontend/src/config/Blocks/index.js
@@ -1,4 +1,5 @@
 import SimpleCardListing from '@package/components/Blocks/Listing/variations/SimpleCardListing';
+import SponsorCardListing from '@package/components/Blocks/Listing/variations/SponsorCardListing';
 import MemberCardListing from '@package/components/Blocks/Listing/variations/MemberCardListing';
 import NewsCardListing from '@package/components/Blocks/Listing/variations/NewsCardListing';
 import DownloadListing from '@package/components/Blocks/Listing/variations/DownloadListing';
@@ -6,6 +7,7 @@ import SearchResults from '@package/components/Blocks/Listing/variations/SearchR
 
 import getDefaultSchema from '@package/config/Blocks/schemas/listing-variations/default-schema';
 import getSimpleCardVariationSchema from '@package/config/Blocks/schemas/listing-variations/simple-card-variation-schema';
+import getSponsorCardVariationSchema from '@package/config/Blocks/schemas/listing-variations/sponsor-card-variation-schema';
 import getNewsCardVariationSchema from '@package/config/Blocks/schemas/listing-variations/news-card-variation-schema';
 import getDownloadVariationSchema from '@package/config/Blocks/schemas/listing-variations/download-list-variation-schema';
 import getSearchResultsVariationSchema from '@package/config/Blocks/schemas/listing-variations/search-results-variation-schema';
@@ -394,6 +396,17 @@ export const SiteBlocks = (config) => {
           schemaEnhancer: ({ schema, formData, intl }) => {
             let pos = getDefaultSchema(schema, formData, intl);
             getSimpleCardVariationSchema(schema, formData, intl, pos);
+            return schema;
+          },
+        },
+        {
+          id: 'sponsorCardListing',
+          isDefault: false,
+          title: 'Foundation Sponsor Card',
+          template: SponsorCardListing,
+          schemaEnhancer: ({ schema, formData, intl }) => {
+            let pos = getDefaultSchema(schema, formData, intl);
+            getSponsorCardVariationSchema(schema, formData, intl, pos);
             return schema;
           },
         },
