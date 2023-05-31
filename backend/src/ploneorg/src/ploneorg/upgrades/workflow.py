33c216a7d993b686ac39b7c404ecd81d2c18baa1
Érico Andrei <ericof@gmail.com>
2023-05-30 22:18:03 -0300

    Re-apply specific permissions on /foundation/members/applications

--- backend/src/ploneorg/src/ploneorg/upgrades/workflow.py
+++ backend/src/ploneorg/src/ploneorg/upgrades/workflow.py
@@ -2,8 +2,31 @@ from plone import api
 from ploneorg import logger
 
 
+APPLICATION_ALLOWED_ROLES = [
+    "Authenticated",
+    "Contributor",
+    "Editor",
+    "Manager",
+    "Member",
+    "Reviewer",
+    "Site Manager",
+]
+APPLICATION_ALLOWED_PERMISSIONS = [
+    "Add portal content",
+    "ploneorg: Add Foundation Member",
+]
+
+
 def update_security(context):
     # Update workflow security
     wf_tool = api.portal.get_tool("portal_workflow")
     wf_tool.updateRoleMappings()
     logger.info("Updated security")
+    # Fix permissions on /foundation/members/applications/
+    path = "/foundation/members/applications"
+    content = api.content.get(path=path)
+    if content:
+        for permission_id in APPLICATION_ALLOWED_PERMISSIONS:
+            content.manage_permission(permission_id, roles=APPLICATION_ALLOWED_ROLES)
+            logger.info(f"Updated {permission_id} in {path}")
+    logger.info("Finished upgrade")
