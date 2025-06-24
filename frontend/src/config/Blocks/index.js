855865213c0d1ac7de7ab1ad79fd0ac5274740a5
David Glick <david@glicksoftware.com>
2025-06-24 10:39:33 -0600

    Remove icons_and_numbers block (unused)

--- frontend/src/config/Blocks/index.js
+++ frontend/src/config/Blocks/index.js
@@ -56,10 +56,6 @@ import BreadcrumbsEdit from '@package/components/Blocks/Breadcrumbs/Edit';
 import ViewHTMLBlock from '@package/components/Blocks/HTML/View';
 import EditHTMLBlock from '@package/components/Blocks/HTML/Edit';
 
-import iconsNumbersSVG from '@package/icons/icons_and_numbers.svg';
-import IconsAndNumbersView from '@package/components/Blocks/IconsAndNumbers/View';
-import IconsAndNumbersEdit from '@package/components/Blocks/IconsAndNumbers/Edit';
-
 import iconsAndTextSVG from '@package/icons/icons_and_text.svg';
 import IconsAndTextView from '@package/components/Blocks/IconsAndText/View';
 import IconsAndTextEdit from '@package/components/Blocks/IconsAndText/Edit';
@@ -302,16 +298,6 @@ export const SiteBlocks = (config) => {
       group: 'text',
       view: NumbersView,
       edit: NumbersEdit,
-      sidebarTab: 1,
-    },
-    icons_and_numbers: {
-      ...defaultBlocksConfig,
-      id: 'icons_and_numbers',
-      title: 'Icons and numbers',
-      icon: iconsNumbersSVG,
-      group: 'text',
-      view: IconsAndNumbersView,
-      edit: IconsAndNumbersEdit,
       sidebarTab: 1,
     },
     icons_and_text: {
