92060dbeb2e006111cb2f4fb26377995b1531d7e
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-02-07 09:44:46 +0100

    feat: add a Call to Action

--- frontend/src/components/FooterSponsors/FooterSponsors.jsx
+++ frontend/src/components/FooterSponsors/FooterSponsors.jsx
@@ -2,6 +2,9 @@ import { searchContent } from '@plone/volto/actions/search/search';
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect } from 'react';
 import SponsorCardListing from '../Blocks/Listing/variations/SponsorCardListing';
+import { Button } from '@package/components';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { UniversalLink } from '@plone/volto/components';
 
 const FooterSponsors = (props) => {
   const { type = 'platinum' } = props;
@@ -29,11 +32,22 @@ const FooterSponsors = (props) => {
   }, [dispatch, subrequest_key, type]);
 
   const plural = sponsors?.items.length > 1 ? 's' : '';
-
+  const capitalizedType = String(type[0]).toUpperCase() + String(type).slice(1);
   return sponsors?.loaded
     ? sponsors.items.length > 0 && (
-        <div class={`footer-sponsors-listing ${subrequest_key}`}>
-          <h3>{`Our proud ${type} sponsor${plural}`}</h3>
+        <div className={`footer-sponsors-listing ${subrequest_key}`}>
+          <div className="footer-sponsors-listing-headline">
+            <h3>{`Our proud ${capitalizedType} Sponsor${plural}`}</h3>
+            <Button
+              as={UniversalLink}
+              primary
+              size="medium"
+              href={flattenToAppURL('/foundation/sponsorship')}
+              arrow={true}
+            >
+              Become a Sponsor
+            </Button>
+          </div>
           <SponsorCardListing items={sponsors.items || []} />
         </div>
       )
