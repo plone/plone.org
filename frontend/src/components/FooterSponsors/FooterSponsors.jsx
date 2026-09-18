8c447e5fdd5c49b304c6e8bc5f250a4d614e906e
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-09-18 16:02:03 +0200

    lint

--- frontend/src/components/FooterSponsors/FooterSponsors.jsx
+++ frontend/src/components/FooterSponsors/FooterSponsors.jsx
@@ -52,7 +52,7 @@ const FooterSponsors = (props) => {
         subrequest_key,
       ),
     );
-  }, [dispatch, subrequest_key, type]);
+  }, [dispatch, subrequest_key, type, sponsor_query]);
 
   const sponsorItems = sponsors?.loaded
     ? sponsors.items.length > 0 &&
